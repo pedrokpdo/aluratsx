963d8b0de56fc713fbf04d4838ee0a727254770b
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-21 14:23:06 -0300

    controlando formularios

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,16 +1,31 @@
+
 import React from "react";
 import Botao from "../Botao";
 import style from "./Formulario.module.scss"
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: "",
+        tempo: "00:00",
+    }
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault()
+        console.log('state: ', this.state);
+
+    }
+
     render() {
         return (
-            <form className={style.novaTarefa}>
+            <form
+                onSubmit={this.adicionarTarefa.bind(this)}
+                className={style.novaTarefa}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">adicione uma nova tarefa</label>
                     <input
                         type="text"
                         name="tarefa"
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value })}
                         id="tarefa"
                         placeholder="o que voce quer estudar"
                         required
@@ -22,6 +37,8 @@ class Formulario extends React.Component {
                         type="time"
                         step="1"
                         name="tempo"
+                        value={this.state.tempo}
+                        onChange={evento => this.setState({ ...this.state, tempo: evento.target.value })}
                         id="tempo"
                         min="00:00:00"
                         max="01:30:00"
@@ -29,8 +46,8 @@ class Formulario extends React.Component {
                     />
                 </div>
                 <Botao>Adicionar</Botao>
-                
-                
+
+
             </form>
         )
     }
