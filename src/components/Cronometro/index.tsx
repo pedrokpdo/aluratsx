e3875b51b986bd4b128108dd7f0ae3842e0d6141
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-21 17:26:06 -0300

    final

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -6,9 +6,10 @@ import { ITarefa } from "../../types/tarefa";
 import { useEffect, useState } from "react";
 
 interface Props {
-    selecionado: ITarefa | undefined
+    selecionado: ITarefa | undefined,
+    finalizarTarefa: () => void
 }
-export default function Cronometro({ selecionado }: Props) {
+export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
 
     const [tempo, setTempo] = useState<number>()
 
@@ -23,6 +24,7 @@ export default function Cronometro({ selecionado }: Props) {
                 setTempo(contador - 1)
                 return regressiva(contador - 1)
             }
+            finalizarTarefa()
         }, 1000)
     }
 
--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -9,11 +9,12 @@ export default function Item({ tarefa, tempo, selecionado, completado, id, selec
 
 
     return (
-        <li className={`${style.item} ${selecionado ? style.itemSelecionado : ""}`} onClick={() => selecionaTarefa({
+        <li className={`${style.item} ${selecionado ? style.itemSelecionado : ""} ${completado ? style.itemCompletado : ""} `} onClick={() => !completado && selecionaTarefa({
             tarefa, tempo, selecionado, completado, id
         })}>
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
+            {completado && <span aria-label="tarefa completada" className={style.concluido}></span>}
         </li>
     )
 }
