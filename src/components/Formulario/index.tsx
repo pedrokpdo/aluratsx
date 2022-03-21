b626c5ddcd73c79b3e1082f491cc275aedb0b4a0
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-21 15:10:38 -0300

    atualizando tarefa

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
+import { v4 as uuidv4 } from "uuid"
 import style from "./Formulario.module.scss"
 
 class Formulario extends React.Component<{
@@ -13,8 +14,19 @@ class Formulario extends React.Component<{
     }
     adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault()
-        this.props.setTarefas( tarefasAntigas =>[ ...tarefasAntigas, {...this.state}] )
-
+        this.props.setTarefas(tarefasAntigas =>
+            [
+                ...tarefasAntigas,
+                {
+                    ...this.state,
+                    selecionado: false,
+                    completado: false,
+                    id: uuidv4()
+                }])
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
     }
 
     render() {
--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { ITarefa } from "../../../types/tarefa";
 import style from "../Lista.module.scss"
 
-export default function Item({ tarefa, tempo }: { tarefa: string, tempo: string }) {
+export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
+    console.log("item atual: ", { tarefa, tempo, selecionado, completado, id } );
     
     return (
         <li className={style.item}>
