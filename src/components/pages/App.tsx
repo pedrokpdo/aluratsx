0bcf497b5d1dbe20f8d6900e9e3fd10b8cfa604d
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-21 15:38:47 -0300

    selecionar tarefa

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -4,12 +4,22 @@ import Cronometro from '../Cronometro';
 import Formulario from '../Formulario';
 import Lista from '../Lista';
 import style from './app.module.scss'
+
 function App() {
-  const [tarefas, setTarefas] = useState<ITarefa[] | []> ([])
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([])
+  const [selecionado, setSelecionado] = useState<ITarefa>()
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada)
+
+  }
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
+      <Lista 
+      tarefas={tarefas}
+      selecionaTarefa={selecionaTarefa}
+      />
       <Cronometro />
     </div>
   );
