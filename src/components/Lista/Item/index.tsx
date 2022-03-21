29a76c29944fc5fd598f14fd9695a2c0c9d70561
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-21 16:03:46 -0300

    conversao de tempo

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { ITarefa } from "../../../types/tarefa";
-import style from "../Lista.module.scss"
+import style from "./item.module.scss"
 
 interface Props extends ITarefa {
     selecionaTarefa: (tarefaSelecionada: ITarefa) => void
 }
 export default function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props) {
-    console.log("item atual: ", { tarefa, tempo, selecionado, completado, id });
+
 
     return (
         <li className={`${style.item} ${selecionado ? style.itemSelecionado : ""}`} onClick={() => selecionaTarefa({
