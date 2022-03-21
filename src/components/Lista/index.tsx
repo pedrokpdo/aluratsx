341f780e3dacb7aa811cc53650d5fd43c0b9ae69
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-21 12:46:27 -0300

    conhecendo styled componets

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.scss"
+import style from "./Lista.module.scss"
 
 function Lista() {
     const tarefas = [{
@@ -16,12 +16,12 @@ function Lista() {
     }
     ]
     return (
-        <aside className="listaTarefas">
+        <aside className={style.listaTarefas}>
             <h2>estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <li key={index}
-                        className="item">
+                        className={style.item}>
                         <h3>{item.tarefa}</h3>
                         <span>{item.tempo}</span>
                     </li>
